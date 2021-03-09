e6c45f10e0fe27457c94bb19c571e39954d6fd6e
Vincent Libeskind <vincent.libeskind@epita.fr>
2021-03-09 15:52:13 +0100

    update tools.h

--- Tools/tools.h
+++ Tools/tools.h
@@ -11,4 +11,6 @@ Uint32 BMP_Get_Pixel(SDL_Surface *image, int x, int y);
 void BMP_Put_Pixel(SDL_Surface *image, int x, int y, Uint32 pixel);
 SDL_Surface* BMP_To_BW(SDL_Surface *image);
 void BMP_Test(SDL_Surface *image, int **tab);
+Uint8 Max(Uint8 r, Uint8 g, Uint8 b);
+Uint8 Min(Uint8 r, Uint8 g, Uint8 b);
 #endif
